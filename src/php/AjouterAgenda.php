3ec66e440a5173632d242fbb850e9b29e3e58bbb
kivircik55 <45488599+kivircik55@users.noreply.github.com>
2021-10-07 14:24:37 +0200

    Update AjouterAgenda.php

--- src/php/AjouterAgenda.php
+++ src/php/AjouterAgenda.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 
 <header>
